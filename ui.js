18d70f89d738cd65c361cf64644ef3c78c63d7f9
Yongkang Zhao <cookieyong@gmail.com>
2020-11-11 15:13:38 -0800

    added some theme css to play with and modified the repo cards display format for better presentation for both pc and mobile display

--- ui.js
+++ ui.js
@@ -6,26 +6,28 @@ class UI {
 
     //show profile
     showProfile(user){
-        // console.log(user);
+        console.log(user);
+        let date = new Date(user.created_at);
         this.profile.innerHTML = `
             <div class="card card-body mb-3 profile">
                 <div class="row">
                     <div class="col-md-3">
                         <img class="img-fluid mb-2" src ="${user.avatar_url}" />
-                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">View Profile</a>
+                        <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-1">View Profile</a>
+                        <div>
+                            <span class="badge badge-primary col-md-11 mr-2 ml-2">Public Repos: ${user.public_repos}</span>
+                            <span class="badge badge-primary col-md-11 mr-2 ml-2">Public Gists: ${user.public_gists}</span>
+                            <span class="badge badge-primary col-md-11 mr-2 ml-2">Public Followers: ${user.followers}</span>
+                            <span class="badge badge-primary col-md-11 mr-2 ml-2">Public Following: ${user.following}</span>
+                        </div>
                     </div>
-                    
                     <div class="col-md-9">
-                        <span class="badge badge-primary">Public Repos: ${user.public_repos}</span>
-                        <span class="badge badge-success">Public Gists: ${user.public_gists}</span>
-                        <span class="badge badge-warning">Public Followers: ${user.followers}</span>
-                        <span class="badge badge-info">Public Following: ${user.following}</span>
-                        <br><br>
                         <ul class="list-group">
+                            <li class="list-group-item">Name: ${user.name}</li>
                             <li class="list-group-item">Company: ${user.company}</li>
                             <li class="list-group-item">Website/Blog: ${user.blog}</li>
                             <li class="list-group-item">Location: ${user.location}</li>
-                            <li class="list-group-item">Member Since: ${user.created_at}</li>
+                            <li class="list-group-item">Member Since: ${(date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear()}</li>
                         </ul>
                     </div>
                 </div>
@@ -35,24 +37,35 @@ class UI {
     }
 
     showRepos(repos){
-
+        console.log(repos);
+        let date;
         let output = '<h3 class="page-heading mb-3">Latest Repos</h3>';
+        output += '<div class="row">';
         repos.forEach((repo)=>{
+            date = new Date(repo.pushed_at);
+            
             output += `
-                <div class="card card-body mb-2">
-                    <div class="row col-md-13">
-                        <div class="col-md-9">
-                            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
-                        </div>
-                        <div class="col-md-3.5">
-                        <span class="badge badge-primary col-md-2.33">Stars: ${repo.stargazers_count}</span>
-                        <span class="badge badge-success col-md-2.33">Watchers: ${repo.watchers_count}</span>
-                        <span class="badge badge-warning col-md-2.33">Forks: ${repo.forks_count}</span>
+                <div class="card card-body col-md-12 mb-2">
+                <div class="row col-md-12">
+                    <span class="heading row col-md-8">
+                        <h5><a href="${repo.html_url}" target="_blank">${repo.name}</a></h5>
+                    </span>
+                    <span class="col-md-4">
+                        <span class="badge badge-primary col-md-3">Stars: ${repo.stargazers_count}</span>
+                        <span class="badge badge-primary col-md-3">Watchers: ${repo.watchers_count}</span>
+                        <span class="badge badge-primary col-md-3">Forks: ${repo.forks_count}</span>
+                    </span>
+                    <div>${(date.getMonth()+1) + '/' + date.getDate() + '/' + date.getFullYear()}</div>
+                </div>
+                    <div>
+                        <div>
+                            ${(repo.description != null) ? repo.description : ''}
                         </div>
                     </div>
                 </div>
             `
         });
+        output += '</div>';
         document.querySelector('#repos').innerHTML = output;
     }
 
